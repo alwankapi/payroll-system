433cc5c04b488ec6387ab658b8d60694af21815a
alwankapi <alwankapi@gmail.com>
2026-07-24 22:18:09 +0700

    fix: update darkMode initialization logic and enable class-based dark mode in Tailwind config

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" 
     x-data="{ 
-        darkMode: localStorage.getItem('darkMode') === 'true',
+        darkMode: localStorage.getItem('darkMode') === 'true' ? true : false,
         sidebarOpen: false 
     }" 
     x-init="$watch('darkMode', val => localStorage.setItem('darkMode', val))" 
